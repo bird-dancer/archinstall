66545d767fdf8bac71112de6ce5597df00306021
Felix Dumbeck <53702818+f-eliks@users.noreply.github.com>
2021-01-14 13:37:15 +0100

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,12 @@
+echo $(ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime)
+echo $(hwclock --systohc)
+echo "de_DE.UTF-8 UTF-8" >> /etc/locale.gen
+echo $(locale-gen)
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+echo "KEYMAP=de-latin1" >> /etc/vconsole.conf
+echo "diplodocus" >> /etc/hostname
+echo "127.0.0.1	localhost
+::1		localhost
+127.0.1.1	diplodocus.localdomain	diplodocus" >> /etc/hosts
+echo $(mkinitcpio -P)
+echo "ready"
