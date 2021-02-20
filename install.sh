58ff979fb56a390d6fe9341d0b53299a4981aa6a
f-eliks <53702818+f-eliks@users.noreply.github.com>
2021-02-20 20:03:35 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,18 @@
 echo $(ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime)
 echo $(hwclock --systohc)
-echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
-echo $(locale-gen)
-echo "LANG=en_US.UTF-8" >> /etc/locale.conf
-echo "KEYMAP=de-latin1" >> /etc/vconsole.conf
-echo "diplodocus" >> /etc/hostname
-echo "127.0.0.1	localhost
+echo 'en_US.UTF-8 UTF-8' >> /etc/locale.gen
+echo 'LANG=en_US.UTF-8' > /etc/locale.conf
+echo 'KEYMAP=de-latin1' > /etc/vconsole.conf
+echo 'lemur' > /etc/hostname
+echo '127.0.0.1	localhost
 ::1		localhost
-127.0.1.1	diplodocus.localdomain	diplodocus" >> /etc/hosts
+127.0.1.1	lemur.localdomain	lemur' >> /etc/hosts
 echo $(mkinitcpio -P)
-echo $(pacman -Syu)
-echo $(pacman -S openssh grub nano sudo)
-echo $(systemctl enable sshd)
-echo $(systemctl start sshd)
-echo $(/boot/grub/grub-mkconfig > /boot/grub/grub.cfg)
-echo $(grub-install /dev/sda)
-echo "\n ready"
+echo $(bootctl --path=/boot install)
+echo 'default arch-*' >> /boot/loader/loader.conf
+echo '
+title Arch Linux
+linux /vmlinuz-linux
+initrd  /amd-ucode.img
+initrd  /initramfs-linux.img
+options root=UUID=
