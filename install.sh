540cbab38794c4152b770b94d94b4f28d076ffd1
felix <git@dumbeck.net>
2021-05-09 20:37:11 +0200

    Ãadded locale-genÃÃ

--- install.sh
+++ install.sh
@@ -1,18 +1,19 @@
-echo $(ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime)
-echo $(hwclock --systohc)
+ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime
+hwclock --systohc
 echo 'en_US.UTF-8 UTF-8' >> /etc/locale.gen
 echo 'LANG=en_US.UTF-8' > /etc/locale.conf
 echo 'KEYMAP=de-latin1' > /etc/vconsole.conf
+locale-gen
 echo 'lemur' > /etc/hostname
 echo '127.0.0.1	localhost
 ::1		localhost
 127.0.1.1	lemur.localdomain	lemur' >> /etc/hosts
-echo $(mkinitcpio -P)
-echo $(bootctl --path=/boot install)
+mkinitcpio -P
+bootctl --path=/boot install
 echo 'default arch-*' >> /boot/loader/loader.conf
 echo 'title Arch Linux
 linux /vmlinuz-linux
 initrd  /amd-ucode.img
 initrd  /initramfs-linux.img
 options root=UUID=' > /boot/loader/entries/arch.conf
-echo $(cd /boot/loader/entries)
+cd /boot/loader/entries
