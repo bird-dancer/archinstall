a81d43a143678d4cddac7377ce60f0d2385119fb
Felix <felix.dumbeck@protonmail.com>
2021-06-07 20:15:23 +0200

    a

--- after.sh
+++ after.sh
@@ -1,11 +1,23 @@
 pacman -S wget curl python go dnsutils mlocate vim nano openssh ranger htop tree which nmap zip unzip
+
 read -p "install desktop[yY]" desktop
-case $desktop in
-	[yY]) pacman -S pipewire pipewire-pulse code docker docker-compose mpv nomacs firefox thunderbird discord flatpak torbrowser-launcher alacritty neofetch
+read -p "install plasma[yY]" plasma
+read -p "install xorg[yY] or wayland[nN]" compositor
+case $compositor in
+	[yY])
+		pacman -S xorg xorg-xinit
+	;;
+	[nN])
+		pacman -S wayland
 	;;
 esac
-read -p "install kde plasma[yY]" plasma
 case $plasma in
-	[yY]) pacman -S plasma sddm dolphin ark
+	[yY])
+		pacman -S pacman -S plasma sddm dolphin ark
 	;;
-esac	
+esac
+case $desktop in
+	[yY])
+		pacman -S pipewire pipewire-pulse code docker docker-compose mpv nomacs firefox thunderbird discord flatpak torbrowser-launcher alacritty neofetch
+	;;
+esac
