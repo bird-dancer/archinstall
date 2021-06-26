da1a2674563abcaaae72d5cafdc4f0521a712f72
Felix <53702818+f-eliks@users.noreply.github.com>
2021-06-26 17:50:29 +0200

    Update after.sh

--- after.sh
+++ after.sh
@@ -7,21 +7,21 @@ read -p "install zsh " zsh
 install="sudo pacman -Syyu wget curl python go dnsutils mlocate vim nano openssh ranger htop tree which nmap zip unzip"
 case $xorg in
 	[yY])
-		install+="xorg xorg-xinit"
+		install+=" xorg xorg-xinit"
 	;;
 esac
 case $plasma in
 	[yY])
-		install+="pipewire pipewire-pulse plasma sddm dolphin ark ntfs-3g"
+		install+=" pipewire pipewire-pulse plasma sddm dolphin ark ntfs-3g"
 	;;
 esac
 case $desktop in
 	[yY])
-		install+="code docker docker-compose mpv nomacs firefox thunderbird discord flatpak torbrowser-launcher alacritty neofetch"
+		install+=" code docker docker-compose mpv nomacs firefox thunderbird discord flatpak torbrowser-launcher alacritty neofetch"
 	;;
 esac
 case $zsh in [yY])
-	install+="zsh zsh-syntax-highlighting zsh-autosuggestions exa awesome-terminal-fonts"
+	install+=" zsh zsh-syntax-highlighting zsh-autosuggestions exa awesome-terminal-fonts"
 	;;
 esac
 $install
