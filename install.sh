df2c5fb720a962d885723b7b3b26336397cf05eb
felix <felix.dumbeck@protonmail.com>
2021-07-04 01:15:08 +0200

    added cpu types

--- install.sh
+++ install.sh
@@ -4,18 +4,20 @@ echo 'en_US.UTF-8 UTF-8' >> /etc/locale.gen
 echo 'LANG=en_US.UTF-8' > /etc/locale.conf
 echo 'KEYMAP=de-latin1' > /etc/vconsole.conf
 locale-gen
-echo 'lemur' > /etc/hostname
-echo '127.0.0.1	localhost
+read -p "device name" name
+echo "$name" > /etc/hostname
+echo "127.0.0.1	localhost
 ::1		localhost
-127.0.1.1	lemur.localdomain	lemur' >> /etc/hosts
+127.0.1.1	$name.localdomain	$name" >> /etc/hosts
 mkinitcpio -P
 bootctl --path=/boot install
+read -p "intel or amd cpu: " cpu
 echo 'default arch-*' >> /boot/loader/loader.conf
-echo 'title Arch Linux
+echo "title Arch Linux
 linux /vmlinuz-linux
-initrd  /amd-ucode.img
+initrd  /$cpu-ucode.img
 initrd  /initramfs-linux.img
-options root=UUID=' > /boot/loader/entries/arch.conf
+options root=UUID=" > /boot/loader/entries/arch.conf
 
 passwd=""
 while [ -z $passwd ]; do
